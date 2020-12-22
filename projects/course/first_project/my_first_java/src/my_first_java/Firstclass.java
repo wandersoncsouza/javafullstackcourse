4e516ef9f05b71f1cc576761bf7ace3240bbcc0f
wandersoncsouza <53883005+wandersoncsouza@users.noreply.github.com>
2020-12-22 21:20:51 -0200

    Aula - 60 FirstcodeJava

---
+++ projects/course/first_project/my_first_java/src/my_first_java/Firstclass.java
@@ -0,0 +1,12 @@
+package my_first_java;
+
+public class Firstclass {
+	
+	/*Main é um metodo autoexecutavel em Java*/
+	public static void main(String[] args) {
+		
+		//Escreve o texto no console
+		System.out.println("Meu primeiro projeto em Java");
+	}
+
+}
