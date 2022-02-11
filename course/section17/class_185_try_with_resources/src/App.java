43b217265faa0be0801cac7511e0f875576c3694
wandersoncsouza <w_costasouza@hotmail.com>
2022-02-11 16:56:13 -0300

    class_185_try_with_resources

---
+++ course/section17/class_185_try_with_resources/src/App.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) {
+        String path = ("/Users/wandersoncs/Desktop/Cursos/java/Udemy/nelioalves/javafullstackcourse/course/section17/class_184_file_reader_buffer/src/file.txt");
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }finally{
+            try{
+                if(br != null){
+                    br.close();
+                }
+                if(fr != null){
+                    fr.close();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
