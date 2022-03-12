c90a3897ead878fd4d64bfb02e4e0aa28422138b
wandersoncsouza <w_costasouza@hotmail.com>
2022-03-12 16:40:37 -0300

    class_223_generics
    Permite reuso, segurança de tipos e performance

---
+++ course/section19/class_223/src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Scanner;
+import entities.PrintService;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        Integer x = ps.first();
+        System.out.println("First: " + x);
+        sc.close();
+    }
+}
