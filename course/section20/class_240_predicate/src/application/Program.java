929756ba8c6da2accd78d98149026948574f6638
wandersoncsouza <w_costasouza@hotmail.com>
2022-03-29 14:01:00 -0300

    class_240_predicate_by_interface_funcional

---
+++ course/section20/class_240_predicate/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import entities.Product;
+import util.ProductPredicate;
+
+public class Program {
+    public static void main(String[] args) {
+        // Fazer um programa que, a partir de uma lista de produtos, remova da lista somente
+        // aqueles cujo preço seja 100
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.90));
+
+        // Implementado por instanciação de uma interface funcional
+        list.removeIf(new ProductPredicate());
+
+
+        // Predicato por meio de expresão lambda
+        //list.removeIf(p -> p.getPrice() >= 100);
+
+        for(Product p : list){
+            System.out.println(p);
+        }
+    }
+}
