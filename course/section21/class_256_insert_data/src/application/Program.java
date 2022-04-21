dd7942cbf9e6d36b4c3a9f919073eb6e2179fcc3
wandersoncsouza <w_costasouza@hotmail.com>
2022-04-21 12:57:29 -0300

    class_256_insert_data_returne_Id

--- course/section21/class_256_insert_data/src/application/Program.java
+++ course/section21/class_256_insert_data/src/application/Program.java
@@ -23,7 +23,8 @@ public class Program {
 				"INSERT INTO seller "
 				+ "(Name, Email, BirthDate, Basesalary, DepartmentId) "
 				+ "VALUES "
-				+ "(?, ?, ?, ?, ?)");
+				+ "(?, ?, ?, ?, ?)",
+				Statement.RETURN_GENERATED_KEYS);
 
 
 			st.setString(1, "Carl Purple");
@@ -33,7 +34,15 @@ public class Program {
 			st.setInt(5, 4);
 			
 			int rowsAffected = st.executeUpdate();
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if(rowsAffected > 0){
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()){
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}else{
+				System.out.println("No rows affected!");
+			}
 		}
 		catch(SQLException e){
 			e.printStackTrace();
