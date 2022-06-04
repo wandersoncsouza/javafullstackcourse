161115824ecfa16a4e2d92b7abd2abcadce2ce55
wandersoncsouza <w_costasouza@hotmail.com>
2022-06-04 11:47:51 -0300

    class_270_Insert_Implementation

--- course/section21/class_261_projeto_DAO/src/application/Program.java
+++ course/section21/class_261_projeto_DAO/src/application/Program.java
@@ -1,4 +1,5 @@
 package application;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -26,5 +27,10 @@ public class Program {
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("==== TEST 4: Seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
