775d59d1bb7713e4425e3d18a076ff2b3627197a
wandersoncsouza <w_costasouza@hotmail.com>
2022-06-04 10:06:57 -0300

    class_267_reusing_instantiation

--- course/section21/class_261_projeto_DAO/src/application/Program.java
+++ course/section21/class_261_projeto_DAO/src/application/Program.java
@@ -8,12 +8,9 @@ import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
-        
-        // Department obj = new Department(1, "Books");
-        // Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-        // System.out.println(seller);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println("==== TEST 1: Seller findById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
