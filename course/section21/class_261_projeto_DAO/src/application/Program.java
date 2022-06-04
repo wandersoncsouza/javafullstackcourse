778d73727e69fee0bcc522174876054beaad0cd9
wandersoncsouza <w_costasouza@hotmail.com>
2022-06-04 12:28:19 -0300

    class_272_delete_implementation

--- course/section21/class_261_projeto_DAO/src/application/Program.java
+++ course/section21/class_261_projeto_DAO/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -9,6 +10,8 @@ import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("==== TEST 1: Seller findById ====");
@@ -33,10 +36,17 @@ public class Program {
         // sellerDao.insert(newSeller);
         // System.out.println("Inserted! New id = " + newSeller.getId());
 
-        System.out.println("==== TEST 6: Seller update ====");
-        seller = sellerDao.findById(1);
-        seller.setName("Martha Waine");
-        sellerDao.update(seller);
-        System.out.println("Update completed");
+        // System.out.println("==== TEST 6: Seller update ====");
+        // seller = sellerDao.findById(1);
+        // seller.setName("Martha Waine");
+        // sellerDao.update(seller);
+        // System.out.println("Update completed");
+
+        System.out.println("==== TEST 7: Seller delete ====");
+        System.out.println("Enter id for delete:");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deleted completed!");
+        sc.close();
     }
 }
--- course/section21/class_261_projeto_DAO/src/model/dao/impl/SellerDaoJDBC.java
+++ course/section21/class_261_projeto_DAO/src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLPermission;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -92,7 +93,17 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ? ");
+            st.setInt(1, id);
+            st.executeUpdate();
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally{
+            DB.closeStatement(st);
+        }
         
     }
 
