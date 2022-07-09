69b08ef54013a7c4677a2bbf3792f44549afc5df
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-09 11:43:13 -0300

    class_312_SellerList
    Checklist:
     Clone SellerService
    o Replace: Department -> Seller
     Clone SellerListController
    o Replace: Department -> Seller
    o Comment createDialogForm
     Clone SellerList.fxml
    o Replace: Department -> Seller
     Update MainViewController.onMenuItemSellerAction

--- course/section23/class_289_workshop_javafx_jdbc/src/gui/MainViewController.java
+++ course/section23/class_289_workshop_javafx_jdbc/src/gui/MainViewController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable {
 
@@ -30,7 +31,10 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
