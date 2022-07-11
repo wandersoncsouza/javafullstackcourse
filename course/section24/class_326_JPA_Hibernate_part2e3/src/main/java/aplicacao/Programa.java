41c5e68d4e519737e1dcc635d568831a8c17a062
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-11 10:44:11 -0300

    class_327_jpa_hibernate_searching_and_deleting_data_part3de3

--- course/section24/class_326_JPA_Hibernate_part2e3/src/main/java/aplicacao/Programa.java
+++ course/section24/class_326_JPA_Hibernate_part2e3/src/main/java/aplicacao/Programa.java
@@ -12,18 +12,33 @@ public class Programa {
 		
 		// INSERINDO NO BANCO DE DADOS E CRIANDO AS TABELAS
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Gisele Nascimento", "giselenasc@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Flavio Ribeiro", "flavioribeiro@gmail.com");
+//		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+//		Pessoa p2 = new Pessoa(null, "Gisele Nascimento", "giselenasc@gmail.com");
+//		Pessoa p3 = new Pessoa(null, "Flavio Ribeiro", "flavioribeiro@gmail.com");
+//		
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+//		EntityManager em = emf.createEntityManager();
+//		
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+//		System.out.println("Pronto!!!");
+//		em.close();
+//		emf.close();
+		
+		// BUSCANDO UM DADO NO BANCO DE DADOS
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		em.remove(p);
+		
+		System.out.println(p);
+		
 		System.out.println("Pronto!!!");
 		em.close();
 		emf.close();
