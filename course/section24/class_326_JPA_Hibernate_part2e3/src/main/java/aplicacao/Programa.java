eaff10246aaff1d057577e0a1ab287cd59a7b75b
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-11 10:20:39 -0300

    class_327_jpa_hibernate_part2de3

---
+++ course/section24/class_326_JPA_Hibernate_part2e3/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,33 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		// INSERINDO NO BANCO DE DADOS E CRIANDO AS TABELAS
+		
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Gisele Nascimento", "giselenasc@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Flavio Ribeiro", "flavioribeiro@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto!!!");
+		em.close();
+		emf.close();
+		
+	}
+
+}
