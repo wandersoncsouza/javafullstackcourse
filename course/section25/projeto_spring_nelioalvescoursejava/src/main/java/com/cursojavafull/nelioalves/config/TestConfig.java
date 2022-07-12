3766a801e1804595bbf1cc4ff8100045bd62abe9
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-12 19:38:26 -0300

    class_336_Order, Instant, ISO 8601 - Part 2
    Basic new entity checklist:
     Entity
    o "To many" association, lazy loading, JsonIgnore
     Repository
     Seed
     Service
     Resource
    
    Objects:
    Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
    Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
    Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);

--- course/section25/projeto_spring_nelioalvescoursejava/src/main/java/com/cursojavafull/nelioalves/config/TestConfig.java
+++ course/section25/projeto_spring_nelioalvescoursejava/src/main/java/com/cursojavafull/nelioalves/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.cursojavafull.nelioalves.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.cursojavafull.nelioalves.entities.Order;
 import com.cursojavafull.nelioalves.entities.User;
+import com.cursojavafull.nelioalves.repositories.OrderRepository;
 import com.cursojavafull.nelioalves.repositories.UserRepository;
 
 @Configuration		// Identifica a classe como classe de configuração (auxiliar)
@@ -16,14 +19,21 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired		// Mecanismo de injeção de depenência do UserRepository
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
 		
+		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2,o3));
 	}
 
 	
