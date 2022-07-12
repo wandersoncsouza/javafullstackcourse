2aba751e46828b2205eaf679af8a42d30d6b86b8
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-12 18:39:47 -0300

    class_334_Servicelayer_componentregistration

---
+++ course/section25/projeto_spring_nelioalvescoursejava/src/main/java/com/cursojavafull/nelioalves/services/UserService.java
@@ -0,0 +1,27 @@
+package com.cursojavafull.nelioalves.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursojavafull.nelioalves.entities.User;
+import com.cursojavafull.nelioalves.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired	// injeçao de dependencia do Spring
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
