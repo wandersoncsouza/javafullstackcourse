b707293688e46b46bde3bd17ae10c368865919c8
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-14 13:00:02 -0300

    class_348_user_update
    Checklist:
     UserService
     UserResource
    
    Test:
    {
    "name": "Bob Brown",
    "email": "bob@gmail.com",
    "phone": "977557755"
    }

--- course/section25/projeto_spring_nelioalvescoursejava/src/main/java/com/cursojavafull/nelioalves/services/UserService.java
+++ course/section25/projeto_spring_nelioalvescoursejava/src/main/java/com/cursojavafull/nelioalves/services/UserService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.cursojavafull.nelioalves.entities.User;
 import com.cursojavafull.nelioalves.repositories.UserRepository;
@@ -31,5 +33,17 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
