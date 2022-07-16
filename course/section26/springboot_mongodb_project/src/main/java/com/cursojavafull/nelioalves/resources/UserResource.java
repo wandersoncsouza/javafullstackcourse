b57d161e71ef88190fec9f92d101d3bf87534f23
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-16 00:06:17 -0300

    class_368_entity_user_rest

---
+++ course/section26/springboot_mongodb_project/src/main/java/com/cursojavafull/nelioalves/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.cursojavafull.nelioalves.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursojavafull.nelioalves.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@RequestMapping(method = RequestMethod.GET)		// Mesmo de @GetMapping
+	public  ResponseEntity<List<User>> findAll(){
+		User maria = new User("1", "Maria Brown", "mariabrown@gmail.com");
+		User alex = new User("2", "Alex Green", "alexgreen@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+}
