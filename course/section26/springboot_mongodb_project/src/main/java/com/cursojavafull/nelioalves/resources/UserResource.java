ec3d681c14ed9f234aef130d960ff7503d05a11f
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-16 17:58:55 -0300

    class_Usando padrão DTO para retornar usuários
    Referências:
    https://pt.stackoverflow.com/questions/31362/o-que-é-um-dto
    
    DTO (Data Transfer Object): é um objeto que tem o papel de carregar dados das entidades de forma simples,
    podendo inclusive "projetar" apenas alguns dados da entidade original.
    
    Vantagens:
    - Otimizar o tráfego (trafegando menos dados)
    - Evitar que dados de interesse exclusivo do sistema fiquem sendo expostos (por exemplo: senhas, dados de
    auditoria como data de criação e data de atualização do objeto, etc.)
    - Customizar os objetos trafegados conforme a necessidade de cada requisição (por exemplo: para alterar
    um produto, você precisa dos dados A, B e C; já para listar os produtos, eu preciso dos dados A, B e a
    categoria de cada produto, etc.).
    
    Checklist:
     No subpacote dto, criar UserDTO
     Em UserResource, refatorar o método findAll

--- course/section26/springboot_mongodb_project/src/main/java/com/cursojavafull/nelioalves/resources/UserResource.java
+++ course/section26/springboot_mongodb_project/src/main/java/com/cursojavafull/nelioalves/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.cursojavafull.nelioalves.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursojavafull.nelioalves.domain.User;
+import com.cursojavafull.nelioalves.dto.UserDTO;
 import com.cursojavafull.nelioalves.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)		// Mesmo de @GetMapping
-	public  ResponseEntity<List<User>> finAll(){
+	public  ResponseEntity<List<UserDTO>> finAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x ->  new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
