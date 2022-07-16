6b035b673fd4f4fb4939637cf3a9473f23250bb7
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-16 18:44:25 -0300

    class_372_Obtendo um usuário por id
    import java.util.Optional;
    (...)
    public User findById(String id) {
    Optional<User> obj = repo.findById(id);
    return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
    }
    
    Checklist:
     No subpacote service.exception, criar ObjectNotFoundException
     Em UserService, implementar o método findById
     Em UserResource, implementar o método findById (retornar DTO)
     No subpacote resources.exception, criar as classes:
    o StandardError
    o ResourceExceptionHandler

---
+++ course/section26/springboot_mongodb_project/src/main/java/com/cursojavafull/nelioalves/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.cursojavafull.nelioalves.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.cursojavafull.nelioalves.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice 			// Indica que a classe é para tratar possiveis erros nas requisições
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
