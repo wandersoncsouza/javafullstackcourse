cc65a3bc3d2ecb406f17f0e6f3fc92c5749b4655
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-15 23:42:38 -0300

    class_367_firts_commit_springBoot_project

---
+++ course/section26/springboot_mongodb_project/src/main/java/com/cursojavafull/nelioalves/SpringbootMongodbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cursojavafull.nelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbProjectApplication.class, args);
+	}
+
+}
