8a610ee2f1c2c3826e7248041fdc70f4f054f322
wandersoncsouza <w_costasouza@hotmail.com>
2022-07-18 12:03:41 -0300

    class_380_Endpoint para retornar os posts de um usuário
    Checklist:
     Em UserResource, criar o método para retornar os posts de um dado usuário

--- course/section26/springboot_mongodb_project/src/main/java/com/cursojavafull/nelioalves/resources/UserResource.java
+++ course/section26/springboot_mongodb_project/src/main/java/com/cursojavafull/nelioalves/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.cursojavafull.nelioalves.domain.Post;
 import com.cursojavafull.nelioalves.domain.User;
 import com.cursojavafull.nelioalves.dto.UserDTO;
 import com.cursojavafull.nelioalves.services.UserService;
@@ -59,6 +60,11 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 }
 
